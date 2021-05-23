90e189deb214ba6e9ce29b289ce28c98d9e3b1d7
Olufunto Oyewole <funti009@gmail.com>
2021-05-23 04:28:51 +0100

    made the hero image and content hidden when youtube plays

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,40 +1,35 @@
 import React,{useState,useEffect,useRef} from 'react';
 
-
-
 const HeroSection=({theData})=>{
 	const [presentYoutube,setPresentYoutube]=useState();//this is for the Youtube URL's
 	const  heroImage= useRef(null);//this references the full image that shows
 	const  heroContent= useRef(null);//this references contents on the hero section
-	const daYou=useRef(null);
+	const theIframe=useRef(null);
 	
 	
 	const randomGenerator=()=>{
-		let newIndex= Math.floor(Math.random()*theData.length);//picks a random number
-		heroImage.current.src=theData[newIndex].mainImage.asset.url;//changes the hero image that shows
-		// presentYoutube=theData[newIndex].youtube;//changes the Youtube video source
+		let newData= theData.slice(0,7);
+		console.log(newData)
+		let newIndex= Math.floor(Math.random()*newData.length);//picks a random number
+		heroImage.current.src=newData[newIndex].mainImage.asset.url;//changes the hero image that shows
 		
-		setPresentYoutube(theData[newIndex].URL)//sets the Youtube video url/source
-		// "./img/hero-glasses.png"
-		// console.log(newIndex,imageCatalogue[newIndex].picture )
+		setPresentYoutube(newData[newIndex].URL)//sets the Youtube video url/source
 	}
 	
 	const changeYoutubeVideo=()=>{
-		// daYou.current.src='https://www.youtube.com/embed/qJhyhaf_uRs?autoplay=1'
-		// console.log('yam',daYou)
 		
-		
+		//RESTRUCTURE THE YOUTUBE LINK FROM 'WATCH' TO 'EMBED'
 		let newNumb=presentYoutube.indexOf('watch?v=')+8;
 		let newExtract=presentYoutube.slice(newNumb,newNumb+11);//this extracts the Youtube videoID
 		
 		let newURL= `https://www.youtube.com/embed/${newExtract}?autoplay=1`
 		console.log(newExtract,newURL)
-		daYou.current.src=newURL
+		theIframe.current.src=newURL
+		heroContent.current.classList.add('inactive');//hides the hero contents
+		heroImage.current.classList.add('inactive');//hides the hero Image
 	}
-	
-// https://www.youtube.com/watch?v=dR_Fol8nAzo&t=3369s
-	
 
+	
 	useEffect(()=>{
 		randomGenerator();
 		
@@ -45,13 +40,7 @@ const HeroSection=({theData})=>{
 	return(
 		<section className="hero-section" >
 		
-			<div className="top-youtube " id="video-placeholder" ><iframe  ref={daYou} src='https://www.youtube.com/embed/E7wJTI-1dvQ?'
-				frameborder='0'
-				allow='autoplay; encrypted-media'
-				autoplay='1'
-				allowfullscreen
-				title='video'
-				
+			<div className="top-youtube " id="video-placeholder" ><iframe  ref={theIframe} src='https://www.youtube.com/embed/XqUQbAUnIkc?'
 			/></div>
 			
 			
